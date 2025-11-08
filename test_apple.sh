91bd08f56e9ba6abc458cf0e9587c4107b4b9e19
Jacob Bell <gostonez@yahoo.com>
2025-11-08 12:28:54 -0700

    Add ImageIO framework to test_apple.sh. It's not needed on Mac, but is on iOS.

--- test_apple.sh
+++ test_apple.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-clang -g -x objective-c++ -arch arm64 test.cpp -framework CoreImage -framework AppKit -o test
+clang -g -x objective-c++ -arch arm64 test.cpp -framework CoreImage -framework ImageIO -framework AppKit -o test
 ./test
 rm ./test
